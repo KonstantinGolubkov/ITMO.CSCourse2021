e99b90bbfd368e2ab296ced6e5bfdb7c10c3ccd4
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-04-29 00:25:24 +0300

    Added

--- ITMO.CSCourse2021.Lab01_01.Greetings/Program.cs
+++ ITMO.CSCourse2021.Lab01_01.Greetings/Program.cs
@@ -6,10 +6,16 @@ using System.Threading.Tasks;
 
 namespace ITMO.CSCourse2021.Lab01_01.Greetings
 {
-    class Program
+    class Greater
     {
         static void Main(string[] args)
         {
+            string myName;
+
+            Console.WriteLine("Please enter your name");
+            myName = Console.ReadLine();
+            Console.WriteLine("Hello {0}", myName);
+
         }
     }
 }
