c2acfda510b7e1739def9fec9564e4daf2bd9614
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-04-28 23:30:14 +0300

    Add project files.

---
+++ ITMO.CSCourse2021.Lab01_01.Greetings/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2021.Lab01_01.Greetings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
