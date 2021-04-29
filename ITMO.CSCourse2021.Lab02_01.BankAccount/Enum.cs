ac23900f5eeddc4d457cf154ce5ec4240178cb23
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-04-29 22:02:52 +0300

    Add code enum

---
+++ ITMO.CSCourse2021.Lab02_01.BankAccount/Enum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2021.Lab02_01.BankAccount
+{
+    public enum AccountType {Checking, Deposit}
+
+    public class Enum
+    {
+        static void Main(string[] args)
+        {
+            AccountType goldAccount;
+            AccountType platinumAccount;
+
+            goldAccount = AccountType.Checking;
+            platinumAccount = AccountType.Deposit;
+
+            Console.WriteLine("The Customer Account Type is {0}", goldAccount);
+            Console.WriteLine("The Customer Account Type is {0}", platinumAccount);
+        }
+    }
+}
