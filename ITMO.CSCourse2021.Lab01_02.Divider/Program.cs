cae9308609276ddeb40b091b16cd3d0cfc0fed4e
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-04-29 20:57:00 +0300

    Add code Divider

--- ITMO.CSCourse2021.Lab01_02.Divider/Program.cs
+++ ITMO.CSCourse2021.Lab01_02.Divider/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace ITMO.CSCourse2021.Lab01_02.Divider
 {
-    class Program
+    class DivideIt
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine("Please enter first integer");
+                string temp = Console.ReadLine();
+                int i = Int32.Parse(temp);
+
+                Console.WriteLine("Please enter second integer");
+                temp = Console.ReadLine();
+                int j = Int32.Parse(temp);
+
+                int k = i / j;
+                Console.WriteLine("The result of dividing {0} by {1} is {2}", i, j, k);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An exception was thrown: {0}", e);
+            }
         }
     }
 }
