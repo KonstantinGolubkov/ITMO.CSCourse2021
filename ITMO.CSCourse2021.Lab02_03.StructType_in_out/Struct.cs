a58d4b5f26f2e7b2ac8e972cf6ab6089f1f01602
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-05-01 21:25:01 +0300

    Add project StructType_in_out

---
+++ ITMO.CSCourse2021.Lab02_03.StructType_in_out/Struct.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2021.Lab02_03.StructType_in_out
+{
+        public enum AccountType { Checking, Deposit }
+
+    public struct BankAccount
+    {
+        public long accNo;
+        public decimal accBal;
+        public AccountType accType;
+    }
+         
+    class Struct
+    {
+        static void Main(string[] args)
+        {
+            BankAccount goldAccount;
+
+            goldAccount.accType = AccountType.Checking;
+            goldAccount.accBal = (decimal)3200.00;
+            Console.Write("Enter account number: ");
+            goldAccount.accNo = long.Parse(Console.ReadLine());
+
+            Console.WriteLine("*** Account Summary ***");
+            Console.WriteLine("Acct Number {0}", goldAccount.accNo);
+            Console.WriteLine("Acct Type {0}", goldAccount.accType);
+            Console.WriteLine("Acct Balance ${0}", goldAccount.accBal);
+        }
+    }
+}
