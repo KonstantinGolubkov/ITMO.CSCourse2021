b85c8336d5929ee70ece8d9e71527b28c877243c
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-05-02 00:31:58 +0300

    Replace if else with foreach

--- ITMO.CSCourse2021.Lab03_01.WhatDay1/WhatDay.cs
+++ ITMO.CSCourse2021.Lab03_01.WhatDay1/WhatDay.cs
@@ -24,6 +24,9 @@ namespace ITMO.CSCourse2021.Lab03_01.WhatDay1
 
     class WhatDay
     {
+        static System.Collections.ICollection DaysInMonths = new int[12]
+        { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
         static void Main(string[] args)
         {
             Console.Write("Please enter a day number between 1 and 365: ");
@@ -31,127 +34,140 @@ namespace ITMO.CSCourse2021.Lab03_01.WhatDay1
             int dayNum = int.Parse(line);
             int monthNum = 0;
 
-            if (dayNum <= 31) // January
+            foreach (int daysInMonth in DaysInMonths)
             {
-                goto End;
-            }
-            else
-            {
-                dayNum -= 31;
-                monthNum++;
-            }
-
-            if (dayNum <= 28) // February
-            {
-                goto End;
-            }
-            else
-            {
-                dayNum -= 28;
-                monthNum++;
+                if (dayNum <= daysInMonth)
+                {
+                    break;
+                }
+                else
+                {
+                    dayNum -= daysInMonth;
+                    monthNum++;
+                }
             }
 
-            if (dayNum <= 31) // March
-            {
-                goto End;
-            }
-            else
-            {
-                dayNum -= 31;
-                monthNum++;
-            }
+                //    if (dayNum <= 31) // January
+                //    {
+                //        goto End;
+                //    }
+                //    else
+                //    {
+                //        dayNum -= 31;
+                //        monthNum++;
+                //    }
 
-            if (dayNum <= 30) // April
-            {
-                goto End;
-            }
-            else
-            {
-                dayNum -= 30;
-                monthNum++;
-            }
+                //    if (dayNum <= 28) // February
+                //    {
+                //        goto End;
+                //    }
+                //    else
+                //    {
+                //        dayNum -= 28;
+                //        monthNum++;
+                //    }
 
-            if (dayNum <= 31) // May
-            {
-                goto End;
-            }
-            else
-            {
-                dayNum -= 31;
-                monthNum++;
-            }
+                //    if (dayNum <= 31) // March
+                //    {
+                //        goto End;
+                //    }
+                //    else
+                //    {
+                //        dayNum -= 31;
+                //        monthNum++;
+                //    }
 
-            if (dayNum <= 30) // June
-            {
-                goto End;
-            }
-            else
-            {
-                dayNum -= 30;
-                monthNum++;
-            }
+                //    if (dayNum <= 30) // April
+                //    {
+                //        goto End;
+                //    }
+                //    else
+                //    {
+                //        dayNum -= 30;
+                //        monthNum++;
+                //    }
 
-            if (dayNum <= 31) // July
-            {
-                goto End;
-            }
-            else
-            {
-                dayNum -= 31;
-                monthNum++;
-            }
+                //    if (dayNum <= 31) // May
+                //    {
+                //        goto End;
+                //    }
+                //    else
+                //    {
+                //        dayNum -= 31;
+                //        monthNum++;
+                //    }
 
-            if (dayNum <= 31) // August
-            {
-                goto End;
-            }
-            else
-            {
-                dayNum -= 31;
-                monthNum++;
-            }
+                //    if (dayNum <= 30) // June
+                //    {
+                //        goto End;
+                //    }
+                //    else
+                //    {
+                //        dayNum -= 30;
+                //        monthNum++;
+                //    }
 
-            if (dayNum <= 30) // September
-            {
-                goto End;
-            }
-            else
-            {
-                dayNum -= 30;
-                monthNum++;
-            }
+                //    if (dayNum <= 31) // July
+                //    {
+                //        goto End;
+                //    }
+                //    else
+                //    {
+                //        dayNum -= 31;
+                //        monthNum++;
+                //    }
 
-            if (dayNum <= 31) // October
-            {
-                goto End;
-            }
-            else
-            {
-                dayNum -= 31;
-                monthNum++;
-            }
+                //    if (dayNum <= 31) // August
+                //    {
+                //        goto End;
+                //    }
+                //    else
+                //    {
+                //        dayNum -= 31;
+                //        monthNum++;
+                //    }
 
-            if (dayNum <= 30) // November
-            {
-                goto End;
-            }
-            else
-            {
-                dayNum -= 30;
-                monthNum++;
-            }
+                //    if (dayNum <= 30) // September
+                //    {
+                //        goto End;
+                //    }
+                //    else
+                //    {
+                //        dayNum -= 30;
+                //        monthNum++;
+                //    }
 
-            if (dayNum <= 31) // December
-            {
-                goto End;
-            }
-            else
-            {
-                dayNum -= 31;
-                monthNum++;
-            }
-        End:
-            MonthName temp = (MonthName)monthNum;
+                //    if (dayNum <= 31) // October
+                //    {
+                //        goto End;
+                //    }
+                //    else
+                //    {
+                //        dayNum -= 31;
+                //        monthNum++;
+                //    }
+
+                //    if (dayNum <= 30) // November
+                //    {
+                //        goto End;
+                //    }
+                //    else
+                //    {
+                //        dayNum -= 30;
+                //        monthNum++;
+                //    }
+
+                //    if (dayNum <= 31) // December
+                //    {
+                //        goto End;
+                //    }
+                //    else
+                //    {
+                //        dayNum -= 31;
+                //        monthNum++;
+                //    }
+                //End:
+                MonthName temp = (MonthName)monthNum;
             string monthName = temp.ToString();
 
             Console.WriteLine("{0} {1}", dayNum, monthName);
