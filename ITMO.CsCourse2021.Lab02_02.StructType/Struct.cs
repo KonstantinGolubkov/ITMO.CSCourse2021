d873899a1bcfca2fa0419f10fe8f68da653bd148
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-05-01 17:08:21 +0300

    Add code StructType

---
+++ ITMO.CsCourse2021.Lab02_02.StructType/Struct.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CsCourse2021.Lab02_02.StructType
+{
+    public enum AccountType { Checking, Deposit }
+
+    public struct BankAccount
+    {
+        public long accNo;
+        public decimal accBal;
+        public AccountType accType;
+    }
+
+    class Struct
+    {
+        static void Main(string[] args)
+        {
+            BankAccount goldAccount;
+
+            goldAccount.accType = AccountType.Checking;
+            goldAccount.accBal = (decimal)3200.00;
+            goldAccount.accNo = 123;
+
+            Console.WriteLine("*** Account Summary ***");
+            Console.WriteLine("Acct Number {0}", goldAccount.accNo);
+            Console.WriteLine("Acct Type {0}", goldAccount.accType);
+            Console.WriteLine("Acct Balance ${0}", goldAccount.accBal);
+        }
+    }
+}
