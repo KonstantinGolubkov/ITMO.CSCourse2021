59539f857d1d4a461bc3051391b774042246b121
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-05-02 11:44:10 +0300

    Add code WhatDay2

---
+++ ITMO.CSCourse2021.Lab03_02.WhatDay2/WhatDay.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2021.Lab03_02.WhatDay2
+{
+    enum MonthName
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+
+    class WhatDay
+    {
+        static System.Collections.ICollection DaysInMonths = new int[12]
+        { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Please enter a day number between 1 and 365: ");
+                string line = Console.ReadLine();
+                int dayNum = int.Parse(line);
+
+                if (dayNum < 1 || dayNum > 365)
+                {
+                    throw new ArgumentOutOfRangeException("Day out of range");
+                }
+
+                int monthNum = 0;
+
+                foreach (int daysInMonth in DaysInMonths)
+                {
+                    if (dayNum <= daysInMonth)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        dayNum -= daysInMonth;
+                        monthNum++;
+                    }
+                }
+
+                MonthName temp = (MonthName)monthNum;
+                string monthName = temp.ToString();
+
+                Console.WriteLine("{0} {1}", dayNum, monthName);
+            }
+            catch (Exception caught)
+            {
+                Console.WriteLine(caught);
+            }
+        }
+    }
+}
