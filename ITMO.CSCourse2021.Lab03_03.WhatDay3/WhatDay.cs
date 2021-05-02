26301a75880b6c761ccf50c988e99398e6a19d7f
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-05-02 12:42:30 +0300

    Leap year check - replace if else with maxDayNum

--- ITMO.CSCourse2021.Lab03_03.WhatDay3/WhatDay.cs
+++ ITMO.CSCourse2021.Lab03_03.WhatDay3/WhatDay.cs
@@ -34,16 +34,18 @@ namespace ITMO.CSCourse2021.Lab03_03.WhatDay3
 
                 bool isLeapYear = (yearNum % 4 == 0) && (yearNum % 100 != 0 || yearNum % 400 == 0);
 
-                if (isLeapYear)
-                {
-                    Console.WriteLine(" IS a leap year");
-                }
-                else
-                {
-                    Console.WriteLine(" is NOT a leap year");
-                }
+                int maxDayNum = isLeapYear ? 366 : 365;
 
-                Console.Write("Please enter a day number between 1 and 365: ");
+                //if (isLeapYear)
+                //{
+                //    Console.WriteLine(" IS a leap year");
+                //}
+                //else
+                //{
+                //    Console.WriteLine(" is NOT a leap year");
+                //}
+
+                Console.Write("Please enter a day number between 1 and {0}: ", maxDayNum);
                 line = Console.ReadLine();
                 int dayNum = int.Parse(line);
 
