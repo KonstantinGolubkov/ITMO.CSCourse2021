0e3d6a0cb1b656aa7779f0daa108de57f0ff2f0d
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-05-02 12:34:49 +0300

    Add code WhatDay with leap year check

---
+++ ITMO.CSCourse2021.Lab03_03.WhatDay3/WhatDay.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2021.Lab03_03.WhatDay3
+{
+    enum MonthName
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+
+    class WhatDay
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Please enter the year: ");
+                string line = Console.ReadLine();
+                int yearNum = int.Parse(line);
+
+                bool isLeapYear = (yearNum % 4 == 0) && (yearNum % 100 != 0 || yearNum % 400 == 0);
+
+                if (isLeapYear)
+                {
+                    Console.WriteLine(" IS a leap year");
+                }
+                else
+                {
+                    Console.WriteLine(" is NOT a leap year");
+                }
+
+                Console.Write("Please enter a day number between 1 and 365: ");
+                line = Console.ReadLine();
+                int dayNum = int.Parse(line);
+
+                if (dayNum < 1 || dayNum > 365)
+                {
+                    throw new ArgumentOutOfRangeException("Day out of range");
+                }
+
+                int monthNum = 0;
+
+                foreach (int daysInMonth in DaysInMonths)
+                {
+                    if (dayNum <= daysInMonth)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        dayNum -= daysInMonth;
+                        monthNum++;
+                    }
+                }
+
+                MonthName temp = (MonthName)monthNum;
+                string monthName = temp.ToString();
+
+                Console.WriteLine("{0} {1}", dayNum, monthName);
+            }
+            catch (Exception caught)
+            {
+                Console.WriteLine(caught);
+            }
+        }
+
+        static System.Collections.ICollection DaysInMonths = new int[12]
+        { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    }
+}
