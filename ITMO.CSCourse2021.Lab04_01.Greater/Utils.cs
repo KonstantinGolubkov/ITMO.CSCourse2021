1afd6eeedb92a48df64ca02b9b2fde98af394879
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-05-04 22:46:35 +0300

    Add class Utils

---
+++ ITMO.CSCourse2021.Lab04_01.Greater/Utils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ITMO.CSCourse2021.Lab04_01.Greater
+{
+    class Utils
+    {
+        // Return the greater of two integer values
+
+        public static int Greater(int a, int b)
+        {
+            if (a > b)
+                return a;
+            else
+                return b;
+        }
+
+    }
+}
