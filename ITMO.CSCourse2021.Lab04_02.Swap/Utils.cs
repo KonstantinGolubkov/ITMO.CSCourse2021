1e299940bdee1a520e2c740b9c3b5618872e2d81
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-05-04 23:16:49 +0300

    Add class Utils

---
+++ ITMO.CSCourse2021.Lab04_02.Swap/Utils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2021.Lab04_02.Swap
+{
+    public class Utils
+    {
+        // Exchange two integers, passed by reference
+
+        public static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+    }
+}
