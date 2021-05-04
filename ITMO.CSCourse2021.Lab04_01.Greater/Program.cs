1da3356ca06f7501df33d4d61d57ff556965cc1d
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-05-04 22:38:36 +0300

    Add project Greater

---
+++ ITMO.CSCourse2021.Lab04_01.Greater/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2021.Lab04_01.Greater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
