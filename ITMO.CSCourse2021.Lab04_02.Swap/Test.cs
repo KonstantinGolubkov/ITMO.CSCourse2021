807a297bb6d35ba169efd442e65788956140b1c4
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-05-04 23:33:21 +0300

    Add class Test

---
+++ ITMO.CSCourse2021.Lab04_02.Swap/Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ITMO.CSCourse2021.Lab04_02.Swap
+{
+    /// This the test harness
+
+    public class Test
+    {
+        public static void Main()
+        {
+            int x;
+            int y;
+
+            Console.WriteLine("Enter first number:");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            y = int.Parse(Console.ReadLine());
+
+            // Test the Swap method
+            Console.WriteLine("Before swap: " + x + "," + y);
+            Utils.Swap(ref x, ref y);
+            Console.WriteLine("After swap: " + x + "," + y);
+
+        }
+    }
+}
--- ITMO.CSCourse2021.Lab04_02.Swap/Utils.cs
+++ ITMO.CSCourse2021.Lab04_02.Swap/Utils.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ITMO.CSCourse2021.Lab04_02.Swap
 {
