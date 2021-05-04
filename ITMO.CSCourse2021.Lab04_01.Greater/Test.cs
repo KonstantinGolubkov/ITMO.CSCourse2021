56d58bce69ff4f785149f2832b45fb6742d171b6
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-05-04 23:00:52 +0300

    Add class Test

---
+++ ITMO.CSCourse2021.Lab04_01.Greater/Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ITMO.CSCourse2021.Lab04_01.Greater
+{
+    /// This the test harness
+
+    public class Test
+    {
+        public static void Main()
+        {
+            int x; 
+            int y; 
+            int greater; // Result from Greater()
+
+            Console.WriteLine("Enter first number:");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            y = int.Parse(Console.ReadLine());
+
+            // Test the Greater( ) method
+            greater = Utils.Greater(x, y);
+            Console.WriteLine("The greater value is " + greater);
+        }
+
+    }
+}
