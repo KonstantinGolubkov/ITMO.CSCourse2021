1f36ebed27a2b372a1a74382c352aa74867f1be6
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-05-05 15:31:02 +0300

    Add class Test

---
+++ ITMO.CSCourse2021.Lab04_03.Factorial/Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2021.Lab04_03.Factorial
+{
+    public class Test
+    {
+        static void Main()
+        {
+            int f;                                                      // Factorial result
+            bool ok;                                                    // Factorial success or failure
+            int x;
+
+            Console.WriteLine("Enter number for factorial:");           // Get input for factorial
+            x = int.Parse(Console.ReadLine());
+
+            ok = Utils.Factorial(x, out f);                             // Test the factorial function
+
+            if (ok)                                                     // Output factorial results
+                Console.WriteLine("Factorial(" + x + ") = " + f);
+            else
+                Console.WriteLine("Cannot compute this factorial");
+        }
+    }
+}
