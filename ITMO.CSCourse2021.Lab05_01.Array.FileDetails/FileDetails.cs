8b180d9bd61b20ff88081cc9993959cd2c58f085
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-05-06 20:22:32 +0300

    Displaying the number of characters, vowels, consonants and lines in a file (run from the command line)

--- ITMO.CSCourse2021.Lab05_01.Array.FileDetails/FileDetails.cs
+++ ITMO.CSCourse2021.Lab05_01.Array.FileDetails/FileDetails.cs
@@ -9,6 +9,34 @@ namespace ITMO.CSCourse2021.Lab05_01.Array.FileDetails
 {
     class FileDetails
     {
+        static void Summarize(char[] contents)
+        {
+            int vowels = 0, consonants = 0, lines = 0;
+
+            foreach (char current in contents)
+            {
+                if (Char.IsLetter(current))
+                {
+                    if ("AEIOUaeiou".IndexOf(current) != -1)
+                    {
+                        vowels++;
+                    }
+                    else
+                    {
+                        consonants++;
+                    }
+                }
+                else if (current == '\n')
+                {
+                    lines++;
+                }
+            }
+            Console.WriteLine("Total no of characters: {0}", contents.Length);
+            Console.WriteLine("Total no of vowels : {0}", vowels);
+            Console.WriteLine("Total no of consonants: {0}", consonants);
+            Console.WriteLine("Total no of lines : {0}", lines);
+        }
+
         static void Main(string[] args)
         {
             //Console.WriteLine(args.Length);
@@ -32,10 +60,12 @@ namespace ITMO.CSCourse2021.Lab05_01.Array.FileDetails
                         contents[i] = (char)reader.Read();
                     }
 
-                    foreach (char ch in contents)
-                    {
-                        Console.Write(ch);
-                    }
+                    //foreach (char ch in contents)
+                    //{
+                    //    Console.Write(ch);
+                    //}
+
+                    FileDetails.Summarize(contents);
                 }
             }
         }   
