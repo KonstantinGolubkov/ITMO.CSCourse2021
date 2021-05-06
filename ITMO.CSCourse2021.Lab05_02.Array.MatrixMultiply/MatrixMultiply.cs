ad8436a9e486db2df515443c294776bd5334957e
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-05-06 22:21:34 +0300

    Upper bound GetLength in method Output

--- ITMO.CSCourse2021.Lab05_02.Array.MatrixMultiply/MatrixMultiply.cs
+++ ITMO.CSCourse2021.Lab05_02.Array.MatrixMultiply/MatrixMultiply.cs
@@ -10,12 +10,9 @@ namespace ITMO.CSCourse2021.Lab05_02.Array.MatrixMultiply
     {
         static void Output(int[,] result)
         {
-            //Console.WriteLine("{0}   {1}", result[0, 0], result[0, 1]);
-            //Console.WriteLine("{0}   {1}", result[1, 0], result[1, 1]);
-
-            for (int r = 0; r < 2; r++)
+            for (int r = 0; r < result.GetLength(0); r++)
             {
-                for (int c = 0; c < 2; c++)
+                for (int c = 0; c < result.GetLength(1); c++)
                 {
                     Console.Write("{0} ", result[r, c]);
                 }
