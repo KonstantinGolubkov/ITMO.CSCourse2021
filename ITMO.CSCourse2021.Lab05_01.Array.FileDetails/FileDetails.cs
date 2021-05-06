41edb142aa9b60d79f1de6c39b59e501aa199b9f
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-05-06 18:02:22 +0300

    No arguments are passed. Array length = 0

---
+++ ITMO.CSCourse2021.Lab05_01.Array.FileDetails/FileDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;                               //Console
+using System.Collections.Generic;
+using System.IO;                            //FileStream, FileReader
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2021.Lab05_01.Array.FileDetails
+{
+    class FileDetails
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args.Length);
+            foreach (string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+        }
+    }
+}
