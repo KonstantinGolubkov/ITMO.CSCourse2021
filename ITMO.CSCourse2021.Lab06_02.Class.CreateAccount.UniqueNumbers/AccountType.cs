81e20e5e8c89b510400deff3d70b4d7259299d15
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-05-07 21:32:52 +0300

    Add project Class.CreateAccount.UniqueNumbers

---
+++ ITMO.CSCourse2021.Lab06_02.Class.CreateAccount.UniqueNumbers/AccountType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2021.Lab06_02.Class.CreateAccount.UniqueNumbers
+{
+    enum AccountType
+    {
+        Checking,
+        Deposit
+    }
+}
