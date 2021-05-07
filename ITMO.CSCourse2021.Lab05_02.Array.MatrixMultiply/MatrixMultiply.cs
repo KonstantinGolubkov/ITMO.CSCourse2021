0209492bf1509c8e31c7b072fa447d4c6a09b176
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-05-07 13:04:18 +0300

    Add reading of matrix values from console

--- ITMO.CSCourse2021.Lab05_02.Array.MatrixMultiply/MatrixMultiply.cs
+++ ITMO.CSCourse2021.Lab05_02.Array.MatrixMultiply/MatrixMultiply.cs
@@ -11,10 +11,6 @@ namespace ITMO.CSCourse2021.Lab05_02.Array.MatrixMultiply
         static int[,] Multiply(int[,] a, int[,] b)
         {
             int[,] result = new int[2, 2];
-            //result[0, 0] = a[0, 0] * b[0, 0] + a[0, 1] * b[1, 0];
-            //result[0, 1] = a[0, 0] * b[0, 1] + a[0, 1] * b[1, 1];
-            //result[1, 0] = a[1, 0] * b[0, 0] + a[1, 1] * b[1, 0];
-            //result[1, 1] = a[1, 0] * b[0, 1] + a[1, 1] * b[1, 1];
 
             for (int r = 0; r < 2; r++)
             {
@@ -42,12 +38,36 @@ namespace ITMO.CSCourse2021.Lab05_02.Array.MatrixMultiply
         static void Main(string[] args)
         {
             int[,] a = new int[2, 2];
-            a[0, 0] = 1; a[0, 1] = 2;
-            a[1, 0] = 3; a[1, 1] = 4;
+            //a[0, 0] = 1; a[0, 1] = 2;
+            //a[1, 0] = 3; a[1, 1] = 4;
+            Console.Write("Enter value a[0,0]\t");
+            string s00 = Console.ReadLine();
+            a[0, 0] = int.Parse(s00);
+            Console.Write("Enter value a[0,0]\t");
+            string s01 = Console.ReadLine();
+            a[0, 1] = int.Parse(s01);
+            Console.Write("Enter value a[0,1]\t");
+            string s10 = Console.ReadLine();
+            a[1, 0] = int.Parse(s10);
+            Console.Write("Enter value a[1,1]\t");
+            string s11 = Console.ReadLine();
+            a[1, 1] = int.Parse(s11);
 
             int[,] b = new int[2, 2];
-            b[0, 0] = 5; b[0, 1] = 6;
-            b[1, 0] = 7; b[1, 1] = 8;
+            //b[0, 0] = 5; b[0, 1] = 6;
+            //b[1, 0] = 7; b[1, 1] = 8;
+            Console.Write("Enter value b[0,0]\t");
+            string t00 = Console.ReadLine();
+            b[0, 0] = int.Parse(t00);
+            Console.Write("Enter value b[0,0]\t");
+            string t01 = Console.ReadLine();
+            b[0, 1] = int.Parse(t01);
+            Console.Write("Enter value b[0,1]\t");
+            string t10 = Console.ReadLine();
+            b[1, 0] = int.Parse(t10);
+            Console.Write("Enter value b[1,1]\t");
+            string t11 = Console.ReadLine();
+            b[1, 1] = int.Parse(t11);
 
             int[,] result = new int[2, 2];
 
