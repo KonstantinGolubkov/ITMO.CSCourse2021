9ca4167869e4d1b134192caa7d921d1d519c6cc6
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-05-07 18:09:29 +0300

    Add project Class.CreateAccount

---
+++ ITMO.CSCourse2021.Lab06_01.Class.CreateAccount/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2021.Lab06_01.Class.CreateAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
