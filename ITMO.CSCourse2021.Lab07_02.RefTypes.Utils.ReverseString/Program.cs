04d159b367abf6d18dc6ec18c03c510b2aec9826
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-05-13 23:31:01 +0300

    Add project RefTypes.Utils.ReverseString

---
+++ ITMO.CSCourse2021.Lab07_02.RefTypes.Utils.ReverseString/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2021.Lab07_02.RefTypes.Utils.ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
