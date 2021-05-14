c54fbb7a47a860c4c74e77dd6d426e42784833b6
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-05-14 22:21:54 +0300

    Add project RefTypes.CopyFileUpper

---
+++ ITMO.CSCourse2021.Lab07_03.RefTypes.CopyFileUpper/CopyFileUpper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ITMO.CSCourse2021.Lab07_03.RefTypes.CopyFileUpper
+{
+    // Class to create an upper case copy of a file
+
+    class CopyFileUpper
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
