2ae5985eca03d5dd75d1fb1c2f65a1db77f49e08
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-05-14 21:19:08 +0300

    Add class Test for testing method Reverse

---
+++ ITMO.CSCourse2021.Lab07_02.RefTypes.Utils.ReverseString/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2021.Lab07_02.RefTypes.Utils.ReverseString
+{
+    public class Test
+    {
+        public static void Main()
+        {
+            string mes;
+            Console.WriteLine("Enter string to reverse:");
+            mes = Console.ReadLine();
+            Utils.Reverse(ref mes);
+            Console.WriteLine(mes);
+        }
+    }
+}
