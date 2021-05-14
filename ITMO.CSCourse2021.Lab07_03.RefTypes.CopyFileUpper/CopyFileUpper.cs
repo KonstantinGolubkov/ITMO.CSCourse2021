f67317a3eeaeac9ca8df06d578d001a722ea41e8
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-05-14 22:27:59 +0300

    Add Variables

--- ITMO.CSCourse2021.Lab07_03.RefTypes.CopyFileUpper/CopyFileUpper.cs
+++ ITMO.CSCourse2021.Lab07_03.RefTypes.CopyFileUpper/CopyFileUpper.cs
@@ -13,7 +13,14 @@ namespace ITMO.CSCourse2021.Lab07_03.RefTypes.CopyFileUpper
     {
         static void Main()
         {
+            string sFrom, sTo;
+            StreamReader srFrom;
+            StreamWriter swTo;
 
+            Console.WriteLine("Enter name input file:");
+            sFrom = Console.ReadLine();
+            Console.WriteLine("Enter name output file:");
+            sTo = Console.ReadLine();
         }
     }
 }
