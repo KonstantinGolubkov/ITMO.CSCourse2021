bf7f474e6a75acef0efd88413f48b378ed9486ad
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-05-15 13:19:49 +0300

    Add source code - BankAccount_CreateAccount_AccountType

--- ITMO.CSCourse2021.Lab08_01.BankAccount_Constructor/AccountType.cs
+++ ITMO.CSCourse2021.Lab08_01.BankAccount_Constructor/AccountType.cs
@@ -6,10 +6,9 @@ using System.Threading.Tasks;
 
 namespace ITMO.CSCourse2021.Lab08_01.BankAccount_Constructor
 {
-    class Program
+    enum AccountType
     {
-        static void Main(string[] args)
-        {
-        }
+        Checking,
+        Deposit
     }
 }
---
+++ ITMO.CSCourse2021.Lab08_01.BankAccount_Constructor/BankAccount.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2021.Lab08_01.BankAccount_Constructor
+{
+    class BankAccount
+    {
+        private long accNo;
+        private decimal accBal;
+        private AccountType accType;
+
+        private static long nextAccNo = 123;
+
+        public void TransferFrom(BankAccount accFrom, decimal amount)
+        {
+            if (accFrom.Withdraw(amount))
+                this.Deposit(amount);
+        }
+
+        public void Populate(decimal balance)
+        {
+            accNo = BankAccount.NextNumber();
+            accBal = balance;
+            accType = AccountType.Checking;
+        }
+
+        public long Number()
+        {
+            return accNo;
+        }
+
+        public decimal Balance()
+        {
+            return accBal;
+        }
+
+        public string Type()
+        {
+            return accType.ToString();
+        }
+
+        private static long NextNumber()
+        {
+            return nextAccNo++;
+        }
+
+        public decimal Deposit(decimal amount)
+        {
+            accBal += amount;
+            return accBal;
+        }
+
+        public bool Withdraw(decimal amount)
+        {
+            bool sufficientFunds = accBal >= amount;
+            if (sufficientFunds)
+            {
+                accBal -= amount;
+            }
+            return sufficientFunds;
+        }
+    }
+}
