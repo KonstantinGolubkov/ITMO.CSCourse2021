c7183a2e7b5494b8ae2ed0f3aff76f6e3a77cfea
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-05-15 12:13:01 +0300

    Add class Test

---
+++ ITMO.CSCourse2021.Lab07_05.RefTyp.Display_Interface/Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2021.Lab07_05.RefTyp.Display_Interface
+{
+    class Test
+    {   
+        static void Main()
+        {
+            int num = 1000;
+            string msg = "Message";
+            Coordinate c = new Coordinate(21.0, 68.0);      //constructor
+
+            Utils.Display(num);
+            Utils.Display(msg);
+            Utils.Display(c);
+        }
+    }
+}
