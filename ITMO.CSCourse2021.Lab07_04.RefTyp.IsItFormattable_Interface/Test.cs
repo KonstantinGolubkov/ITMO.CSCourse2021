7bb3c6004d7124240b7c5b414633a5433d0b7afa
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-05-15 10:58:20 +0300

    Add class Test

---
+++ ITMO.CSCourse2021.Lab07_04.RefTyp.IsItFormattable_Interface/Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2021.Lab07_04.RefTyp.IsItFormattable_Interface
+{
+    public class Test
+    {
+        static void Main()
+        {
+            int i = 4;
+            ulong ul = 56;
+            string s = "Test";
+
+            Console.WriteLine("int: {0}", Utils.IsItFormattable(i));
+            Console.WriteLine("ulong: {0}", Utils.IsItFormattable(ul));
+            Console.WriteLine("String: {0}", Utils.IsItFormattable(s));
+        }
+    }
+}
