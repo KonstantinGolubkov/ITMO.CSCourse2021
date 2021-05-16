32dee2984fa87e0a79a4c096cb123fe059e31b79
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-05-16 20:52:01 +0300

    Add class BankTransaction

---
+++ ITMO.CSCourse2021.Lab08_02.BankAccount.BankTransaction/BankTransaction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class BankTransaction
+{
+    private readonly decimal amount;
+    private readonly DateTime when;
+
+    public decimal Amount()
+    {
+        return amount;
+    }
+
+    public DateTime When()
+    {
+        return when;
+    }
+}
