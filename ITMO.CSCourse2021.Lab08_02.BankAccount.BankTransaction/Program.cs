1d7ebc8ccc03ebdbe971411bc48fa33988a592b7
ky_golubkov@mail.ru <ky_golubkov@mail.ru>
2021-05-16 20:36:28 +0300

    Add project BankAccount.BankTransaction

---
+++ ITMO.CSCourse2021.Lab08_02.BankAccount.BankTransaction/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2021.Lab08_02.BankAccount.BankTransaction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
